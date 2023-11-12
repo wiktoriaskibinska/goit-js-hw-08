c1905a4074d5a482fea1e742d3051e05074150fc
Wiktoria Skibińska <wikas2000@icloud.com>
2023-11-13 00:11:21 +0100

    saving the current playback time

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+// import
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+// selecting elements 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+// function which gets current time of video and sets it in localStorage
+function onPlay() {
+    player.getCurrentTime().then(seconds => {
+        localStorage.setItem('videoplayer-current-time', seconds)
+    })
+}; 
+
+//alternative function (it uses 'seconds' property from timeupdate event)
+/*function onPlay({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+}
+*/
+
+// watching time update
+player.on('timeupdate', throttle(onPlay, 1000))  
+ 
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+//
+
+ 
