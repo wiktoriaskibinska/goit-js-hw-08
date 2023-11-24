3fdd39fd76f609dec7b73e6e0f955f17d44ebe43
Wiktoria Skibińska <wikas2000@icloud.com>
2023-11-24 22:11:10 +0100

    fixed console error in video player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -20,9 +20,14 @@ function onPlay() {
 */
 
 // watching time update
-player.on('timeupdate', throttle(onPlay, 1000))  
- 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+player.on('timeupdate', throttle(onPlay, 1000)); 
+
+if (localStorage.getItem('videoplayer-current-time') !== null) {
+  player.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+}
+else {
+  player.setCurrentTime(0)
+};
 //
 
  
